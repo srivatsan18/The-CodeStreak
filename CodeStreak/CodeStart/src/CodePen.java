86a45ea211fa7d9607916610db6ce937e7ec5376
Srivatsan Murali <50999780+srivatsan18@users.noreply.github.com>
2023-07-08 22:26:20 +0530

    Kicking of Code Streak

---
+++ CodeStreak/CodeStart/src/CodePen.java
@@ -0,0 +1,9 @@
+public interface CodePen {
+    long numberOfQuestionsSolved;
+    long Streak;
+    String [] topics ;
+
+    void setNumberOfQuestionsSolved();
+    void setStreak();
+    void setTopics();
+}
